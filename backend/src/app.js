a04265daa57f7815e79a088921760e0fdae872bc
MizaelGbb <165435686+MizaelGbb@users.noreply.github.com>
2026-07-06 02:36:37 -0300

    Add files via upload

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,10 @@ const cadastroProduto = require("./routes/produtos/produtosRoutes");
 const cadastroCategoria = require("./routes/produtos/categoriasRoutes");
 const cadastroFornecedor = require("./routes/comprasRoutes/fornecedorRoutes");
 const cadastroVenda = require("./routes/vendas/vendaRoutes");
+const cadastroDesconto = require("./routes/vendas/descontos/descontoRoutes");
+const cadastroCompra = require("./routes/comprasRoutes/compraRoutes");
+const cadastroDescontoCategoria = require("./routes/vendas/descontos/descontoCategoriaRoutes");
+const cadastroDescontoProduto = require("./routes/vendas/descontos/descontoProdutoRoutes");
 const cors = require("cors");
 //const { default: CadastrarCategoria } = require("../../frontend/src/pages/CadastrarCategoria");
 app.use(cors());
@@ -22,6 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 
 
+
 app.use("/cadastra", cadastroCliente);
 app.use("/cadastra", cadastroVendedor);
 app.use("/cadastra", cadastroUsuario);
@@ -29,6 +34,10 @@ app.use("/cadastra", cadastroProduto);
 app.use("/cadastra", cadastroCategoria);
 app.use("/cadastra", cadastroVenda);
 app.use("/cadastra", cadastroFornecedor);
+app.use("/cadastra", cadastroCompra);
+app.use("/cadastra", cadastroDesconto);
+app.use("/cadastra", cadastroDescontoCategoria);
+app.use("/cadastra", cadastroDescontoProduto);
 app.use("/auth", authRoutes);
 
 
